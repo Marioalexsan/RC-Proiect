2f31e9d9d81a99c91c4c5021086ab188221b7af0
Miron Alexandru <mironalex@hotmail.com>
2021-10-14 17:58:11 +0300

    Edited main.py slightly

--- src/main.py
+++ src/main.py
@@ -1,5 +1,22 @@
+from tkinter import *
+from tkinter import ttk
+
+
 def main():
-    print('Hello world!')
+    print('Starting application!')
+
+    root = Tk()
+    frame = ttk.Frame(root, padding=10, width=800, height=600)
+
+    frame.grid()
+    ttk.Label(frame, text='Hello World!').grid(column=0, row=0)
+    ttk.Button(frame, text='Quit', command=root.destroy).grid(column=1, row=0)
+
+    frame.pack()
+    root.mainloop()
+
+    print('Quitting application!')
+    return
 
 
 if __name__ == '__main__':
